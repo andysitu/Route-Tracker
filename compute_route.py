d096c2070e8d7b3740aef50db812758cee6294d4
Andy Situ <and.situ@gmail.com>
2025-12-06 23:54:18 -0800

    Print out route as JSON with traffic

--- compute_route.py
+++ compute_route.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv
 
 from google.maps.routing_v2 import RoutesAsyncClient
 from google.maps.routing_v2.types import (
-    ComputeRoutesRequest, 
-    Waypoint, 
-    Location, 
-    RouteTravelMode
+    ComputeRoutesRequest,
+    Waypoint,
+    Location,
+    RouteTravelMode,
+    RoutingPreference,
 )
 from google.type.latlng_pb2 import LatLng
 from google.protobuf.field_mask_pb2 import FieldMask
@@ -28,26 +29,40 @@ async def compute_route():
         request = ComputeRoutesRequest(
             origin=origin,
             destination=destination,
-            travel_mode=RouteTravelMode.DRIVE
+            travel_mode=RouteTravelMode.DRIVE,
+            routing_preference=RoutingPreference.TRAFFIC_AWARE_OPTIMAL,
         )
 
-        field_mask = FieldMask(paths=["routes.duration", "routes.distance_meters"])
+        paths = [
+            "routes.duration",
+            "routes.distance_meters",
+            "routes.polyline.encoded_polyline",
+            "geocodingResults",
+        ]
+        field_mask = FieldMask(paths=paths)
+        # mask_string = ",".join(paths)
+        mask_string = "*"
 
         try:
             response = await client.compute_routes(
-                request=request,
-                metadata=[("x-goog-fieldmask", field_mask.paths[0])]
+                request=request, metadata=[("x-goog-fieldmask", mask_string)]
             )
 
             if response.routes:
-                route = response.routes[0]
-                print(f"Distaince: {route.distance_meters} meters")
-                print(f"Async Route Duration: {route.duration.seconds} seconds")
+
+                # response_json = json_format.MessageToJson(
+                #     response.routes[0],
+                #     indent=2,  # Use indent=2 for nicely formatted, readable output
+                # )
+                response_json = type(response).to_json(response)
+
+                print("-------- ROUTES API RESPONSE (JSON) --------")
+                print(response_json)
             else:
                 print("No routes found")
 
         except Exception as ex:
-            print(f"An error occurred: {e}")
+            print(f"An error occurred: {ex}")
 
 if __name__ == "__main__":
     asyncio.run(compute_route())
