85b5eabd8537bfa2157460611a1ef4536f6a0481
Andy Situ <and.situ@gmail.com>
2025-12-08 23:05:58 -0800

    save_json expects json but can connect

--- json_lib.py
+++ json_lib.py
@@ -2,13 +2,14 @@ import os
 import json
 
 
-def save_json(json_str, folder_path: str, filename: str):
+def save_json(to_save_json, folder_path: str, filename: str):
     # Make sure the folder exists
     os.makedirs(folder_path, exist_ok=True)
 
     filepath = os.path.join(folder_path, filename)
 
-    python_data = json.loads(json_str)
+    if type(to_save_json) is str:
+        to_save_json = json.loads(to_save_json)
 
     with open(filepath, "w") as file:
-        json.dump(python_data, file, indent=2)
+        json.dump(to_save_json, file, indent=2)
