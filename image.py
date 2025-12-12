29e19ae0da0a18d37ef07f507ca6c1f3f28a1c4c
Andy Situ <and.situ@gmail.com>
2025-12-11 23:02:53 -0800

    Use httpx for request and fix import

--- image.py
+++ image.py
@@ -1,13 +1,26 @@
 import requests
+import httpx
+import os
 
 
-def download_image(encoded_polyline):
+async def download_image(encoded_polyline):
     url = (
         "https://maps.googleapis.com/maps/api/staticmap?"
         f"size=600x400&"
         f"path=color:0x0000FF|weight:5|enc:{encoded_polyline}&"
         f"key={os.environ['GOOGLE_API_KEY']}"
     )
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url, follow_redirects=True)
+            # response.raise_for_status()
+            return response
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP Error fetching static map: {e}")
+            print(f"Response content: {e.response.text}")
+        except httpx.RequestError as e:
+            print(f"An error occurred while requesting the map: {e}")
 
     response = requests.get(url)
 
